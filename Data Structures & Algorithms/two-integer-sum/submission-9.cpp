9610b02c99d92b84ce5259e2202d17d006666e07
Karthik J <120185330+KJ176@users.noreply.github.com>
2026-04-06 22:22:57 +0530

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        for(int i = 0; i < nums.size(); i++) {
+            int req = target - nums[i];
+            if (mpp.find(req) != mpp.end()) {
+                int j = mpp[req];
+                return {min(i, j), max(i, j)};
+            }
+            mpp[nums[i]] = i; 
+        }
+        return {};
+    }
+};
